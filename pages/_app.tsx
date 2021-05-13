793fa821df5fb8ee14deebbac0c290a86bb0f563
jonathan <jonathan.lemaine@hotmail.Fr>
2021-05-13 18:39:50 +0200

    remove wrong import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "tailwindcss/tailwind.css";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <Head>
+          <title>Riskspot</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head >
-                    <link href="https://static.parastorage.com/services/third-party/fonts/Helvetica/fontFace.css" rel="stylesheet" />
-                    <link href="//db.onlinewebfonts.com/c/9a7e574c3eda05d71d03345d3f9e268e?family=FuturaLTW01-LightOblique" rel="stylesheet" type="text/css"/>
-                    <title>Riskspot</title>
                 </Head>
                 <body>
                 <Main />
